feef062ad1aa9ddb67dd0082140efb4587dd9cbe
pitikdmitry <pitik.dmitry@mail.ru>
2017-09-18 16:38:45 +0300

    hw0

--- source/max.js
+++ source/max.js
@@ -1,3 +1,19 @@
 'use strict';
 
 const max = numbers => Math.max(...numbers);
+
+function zip(arr) {
+    let result = {};
+
+    for(let i = 0; i < arguments.length; ++i) {
+        let tempObject = arguments[i];
+
+        for(let key in tempObject) {
+            if(result[key] == undefined) {
+                result[key] = tempObject[key];
+            }
+        }
+    }
+
+    return result;
+}
